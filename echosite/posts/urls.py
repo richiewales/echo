47b507eecd81db6ceea197332912f017d70d57c0
Richie Wales <richiew@utexas.edu>
2023-08-21 03:28:03 -0400

    Add example page with button to generate tweet about catgirls and civics

--- echosite/posts/urls.py
+++ echosite/posts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import index
+from .views import index, generate_post
 urlpatterns = [
     path("", index, name="index"),
+    path('generate-post', generate_post, name='generate-post'),
 ]
--- echosite/posts/views.py
+++ echosite/posts/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from typing import Optional
+from llama_cpp import Llama
+from echosite.settings import BASE_DIR
+import os
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the posts index.")
-    
+
+def generate_post(request):
+    llm = Llama(model_path=os.path.join(BASE_DIR, 'models/Llama-2-7B-Chat-GGML/llama-2-7b-chat.ggmlv3.q4_0.bin'))
+    output = llm("Q: Generate a tweet about catgirls and honda civis.  A: ", max_tokens=128, stop=["Q:", "\n"], echo=True)
+    output = output['choices'][0]['text'].split('A: ')[1]
+    return JsonResponse({'prompt': output})
