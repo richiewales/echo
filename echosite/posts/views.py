cfff1941f6f3c6d5788e459c72eb0f4edc6f6375
Richie Wales <richiew@utexas.edu>
2023-08-25 22:31:04 -0400

    Make prompt dynamic from input box

--- echosite/posts/views.py
+++ echosite/posts/views.py
@@ -10,7 +10,8 @@ def index(request):
     return HttpResponse("Hello, world. You're at the posts index.")
 
 def generate_post(request):
+    topics = ''.join(request.POST['topics'])
     llm = Llama(model_path=os.path.join(BASE_DIR, 'models/Llama-2-7B-Chat-GGML/llama-2-7b-chat.ggmlv3.q4_0.bin'))
-    output = llm("Q: Generate a tweet about catgirls and honda civis.  A: ", max_tokens=128, stop=["Q:", "\n"], echo=True)
+    output = llm("Q: Generate a tweet about {}.  A: ".format(topics), max_tokens=128, stop=["Q:", "\n"], echo=True)
     output = output['choices'][0]['text'].split('A: ')[1]
     return JsonResponse({'prompt': output})
