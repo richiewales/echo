7ec01d1231afac5b39ccd8d7e3957d44b8de868f
Richie Wales <richiew@utexas.edu>
2023-08-27 01:06:06 -0400

    Updated test data

--- echo-client/pages/api/hello.js
+++ echo-client/pages/api/hello.js
@@ -5,31 +5,32 @@ export default function handler(req, res) {
     [{
       profile: 'John Doe',
       profilePicture: "https://placehold.co/55x55",
-      postContent: "🚣‍♀️Just set sail on a boat adventure! The sun, sea, and endless horizons stretch out before me. Life doesn't get much better than this. #boatlife #travelbysea",
+      postContent: "🚀AI can now recognize cat faces! 🐱💡 Learn more about how #ai is enhancing our feline friends' lives on our blog post. Link in bio. #cats #artificialintelligence",
       postTimestamp: "1693103816987"
     },
     {
       profile: 'John Doe',
       profilePicture: "https://placehold.co/55x55",
-      postContent: "🚣‍♀️Just set sail on a boat adventure! The sun, sea, and endless horizons stretch out before me. Life doesn't get much better than this. #boatlife #travelbysea",
+      postContent: "🚀 Elon Musk just revealed his new feline side hustle - he's launching a line of cat-themed merchandise! 🐱✨ Stay tuned for the purrfectly amazing products! #catsofinstagram #felinefun #ElonMusk",
       postTimestamp: "1693103816987"
     },
     {
       profile: 'John Doe',
       profilePicture: "https://placehold.co/55x55",
-      postContent: "🚣‍♀️Just set sail on a boat adventure! The sun, sea, and endless horizons stretch out before me. Life doesn't get much better than this. #boatlife #travelbysea",
+      postContent: "🎮👗 Just found out my fave game character's outfit is trending on Fashion Week! Can't wait to rock their style in the real world 😍 #gamingfashion #styleinspo",
       postTimestamp: "1693103816987"
     },
     {
       profile: 'John Doe',
       profilePicture: "https://placehold.co/55x55",
-      postContent: "🚣‍♀️Just set sail on a boat adventure! The sun, sea, and endless horizons stretch out before me. Life doesn't get much better than this. #boatlife #travelbysea",
+      postContent: "🚀 Traveling with your furry friend?  Don't forget to pack their favorite toys & treats! 🐾🏖️ #travelwithdogs #pettravel #dogfriendly Destination? 🤔",
       postTimestamp: "1693103816987"
     },
     {
       profile: 'John Doe',
       profilePicture: "https://placehold.co/55x55",
-      postContent: "🚣‍♀️Just set sail on a boat adventure! The sun, sea, and endless horizons stretch out before me. Life doesn't get much better than this. #boatlife #travelbysea",
+      postContent: "🚨AI is revolutionizing the fashion industry! From generating customized outfits to predicting future trends, machine learning algorithms are taking style to new heights! #AIinFashion #FutureOfStyle",
       postTimestamp: "1693103816987"
-    }]);
+    }
+  ]);
 }
