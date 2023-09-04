cecbcfca246c365c72e16e99791ce239cc3f34fe
Richie Wales <richiew@utexas.edu>
2023-09-03 23:47:32 -0400

    Add user and post models

--- echosite/posts/models.py
+++ echosite/posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=60)
+    author_id = models.IntegerField(primary_key=True)
+    
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_id = models.CharField(max_length=38, primary_key=True)
+    timestamp = models.DateTimeField()
+    post_text = models.CharField(max_length=300)
