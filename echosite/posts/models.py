f0ff136c1dff0efb57e8bfbf7f245046c9920361
Richie Wales <richiew@utexas.edu>
2023-09-04 03:04:38 -0400

    Updated user model to split fields on serialization

--- echosite/posts/models.py
+++ echosite/posts/models.py
@@ -2,9 +2,20 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    name = models.CharField(max_length=60)
+    name = models.CharField(max_length=60, default='Richie Chungus', null=False)
     author_id = models.IntegerField(primary_key=True)
-    
+    picture = models.URLField(default='https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png')  
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'picture'],
+                name='unique_name_picture'
+            )
+        ]
+    def natural_key(self):
+        return (self.name, self.picture)
+
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     post_id = models.CharField(max_length=38, primary_key=True)
--- echosite/posts/views.py
+++ echosite/posts/views.py
@@ -95,5 +95,5 @@ def consume_posts(request):
 def get_posts(request):
     recent_100_posts = Post.objects.all()[:100]
     print(recent_100_posts)
-    serialized_data = serialize("json", recent_100_posts)
-    return HttpResponse(serialized_data)
+    serialized_data = serialize("json", recent_100_posts, use_natural_foreign_keys=True)
+    return JsonResponse(serialized_data, safe=False)
